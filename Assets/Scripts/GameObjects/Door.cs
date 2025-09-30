f4f8852fcaf9f659a4a7211eb9d6d0fa152e1571
SeongWooJeong <jsw0510@pusan.ac.kr>
2025-09-30 17:13:17 +0900

    feat: 아이템 구현

---
+++ Assets/Scripts/GameObjects/Door.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private string _doorId;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private string _openAnimationName = "Open";
+    [SerializeField] private string _closeAnimationName = "Close";
+    private bool _isOpen = false;
+
+
+    public bool IsOpen => _isOpen;
+
+
+    public void CloseDoor()
+    {
+        Debug.Log("Attempting to close door...");
+        if (!PlayerInventory.Instance.HasItem(_doorId)) return;
+        if (!_isOpen) return;
+
+        PlayerInventory.Instance.RemoveItem(_doorId);
+        _animator.Play(_closeAnimationName);
+        _isOpen = false;
+    }
+}
---
+++ Assets/Scripts/Item/Item.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] private ItemSO _itemData;
+    public ItemSO ItemData => _itemData;
+}
